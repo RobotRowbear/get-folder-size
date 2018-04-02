5520fed05c80ccf7694c78998576036d72e82980
Robert <rwai@osisoft.com>
2018-04-02 00:35:30 -0700

    added option to ignore permissions errors & continue to calculate total

--- index.js
+++ index.js
@@ -4,17 +4,10 @@ var fs = require('fs');
 var path = require('path');
 var async = require('async');
 
-function readSizeRecursive(item, ignoreRegEx, callback) {
-  var cb;
-  var ignoreRegExp;
-
-  if (!callback) {
-    cb = ignoreRegEx;
-    ignoreRegExp = null;
-  } else {
-    cb = callback;
-    ignoreRegExp = ignoreRegEx;
-  }
+function readSizeRecursive(item, callback, ignoreRegEx = null, ignorePermissionsErrors = true) {
+  var cb = callback;
+  var ignoreRegExp = ignoreRegEx;
+  var ignorePermErr = ignorePermissionsErrors
 
   fs.lstat(item, function lstat(e, stats) {
     var total = !e ? (stats.size || 0) : 0;
@@ -28,12 +21,22 @@ function readSizeRecursive(item, ignoreRegEx, callback) {
           function iterate(dirItem, next) {
             readSizeRecursive(
               path.join(item, dirItem),
-              ignoreRegExp,
               function readSize(error, size) {
-                if (!error) { total += size; }
-
-                next(error);
-              }
+                if (!error) {
+                  total += size;
+                  return next();
+                } else {
+                  const code = error.code;
+                  if (ignorePermissionsErrors == true && (code == 'EPERM' || code == 'EACCES')) {
+                    next();
+                  }
+                  else {
+                    next(error);
+                  }
+                }
+              },
+              ignoreRegExp,
+              ignorePermErr
             );
           },
           function done(finalErr) {
